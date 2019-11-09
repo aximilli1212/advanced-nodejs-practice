36a7de30a923232885e4c80ef2619f76ba0e2f4e
aximilli1212 <aximilli1212@gmail.com>
2019-11-09 20:43:18 +0000

    Working link enumeration

--- ultimateWikiPresidents/testWork/regExedFile.js
+++ ultimateWikiPresidents/testWork/regExedFile.js
@@ -2,40 +2,28 @@ const allIn = require('./fullFetch');
 
 const url = "https://google.com";
 const domain = new URL(url).hostname;
-let dset = {url:url,rex:/a/i,depth:3};
+let dset = {url:url,rex:/a/i};
+let depth = 3;
 
-
-const urlPath = (u,d)=>{
+const urlPath = (u)=>{
      const urlObject = new URL(u);
 
     let pathString = urlObject.pathname;
-    return pathString.split('/').length - 1 <= d  ? pathString : null ;
+    return pathString.split('/').length - 1 <= depth  ? pathString : null ;
 };
 
 // Crawl webpages
-const crawlr = (dsetter=>{
-    return allIn(dsetter).then(r=>{
-        for(let i = 0; i <= dsetter.depth; i++){ //getting depth links
+ allIn(dset).then(r=>{
+        for(let i = 0; i <= depth; i++){ //getting depth links
             for(let singleLink of r){
-                let newLevel = urlPath(singleLink,dsetter.depth); // raw split with slash as delimiter
+                let newLevel = urlPath(singleLink); // raw split with slash as delimiter
                 if(newLevel){
-                    console.log(newLevel);
-                    console.log("Shiew");
-                    crawlScrapedLinks({url:domain+newLevel});
+                    console.log({url:domain+newLevel});
                 }
             }
         }
+        console.log(r);
+        // console.log({count:r.length})
     }).catch(err=>{
         console.log(err);
     });
-
-});
-
-
-const crawlScrapedLinks = (url,rex="/a/i",depth=3)=>{
-    crawlr({url:url,rex:rex,depth:depth});
-};
-
-crawlScrapedLinks(url,'/as/i',3);
-
-
