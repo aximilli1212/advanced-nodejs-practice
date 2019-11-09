002265dd9a7e066595d4fbfcffe6d27988784e97
aximilli1212 <aximilli1212@gmail.com>
2019-11-09 20:38:18 +0000

    fixed Broken recursive

--- ultimateWikiPresidents/testWork/fullFetch.js
+++ ultimateWikiPresidents/testWork/fullFetch.js
@@ -8,15 +8,12 @@ const allIn = dset =>{
         // Trim and append domain to orphaned links eg:'/profile'
         return ln.trim().startsWith('http') ? ln : `${dset.url}${ln}`;
     };
-
     return new Promise((resolve, reject) => {
         rp(dset.url)
             .then(function(html) {
                 const matchedUrls = [];
-
                 // fetch links as array with cheerio
                 const mlinks = $('a', html);
-
                 //traverse links, filter for regEX and generate array
                 mlinks.each((i, link) => {
                     if(link.attribs.href.match(dset.rex)){
--- ultimateWikiPresidents/testWork/regExedFile.js
+++ ultimateWikiPresidents/testWork/regExedFile.js
@@ -1,28 +1,41 @@
-const htmlPage = require('./htmlFetch');
 const allIn = require('./fullFetch');
 
-let url = "https://google.com";
-// let dset = {url:url,rex:/as/i};
-let depth = 3;
+const url = "https://google.com";
+const domain = new URL(url).hostname;
+let dset = {url:url,rex:/a/i,depth:3};
 
-// htmlPage(dset).then(r=>{
-//     console.log(r);
-// }).catch(err=>{
-//     console.log(err);
-// });
 
-for(let i = 0; i <= depth; i++){
-    let dset = {url:url,rex:/as/i};
+const urlPath = (u,d)=>{
+     const urlObject = new URL(u);
 
-    allIn(dset).then(r=>{
-        console.log(r);
-        console.log({count:r.length})
+    let pathString = urlObject.pathname;
+    return pathString.split('/').length - 1 <= d  ? pathString : null ;
+};
+
+// Crawl webpages
+const crawlr = (dsetter=>{
+    return allIn(dsetter).then(r=>{
+        for(let i = 0; i <= dsetter.depth; i++){ //getting depth links
+            for(let singleLink of r){
+                let newLevel = urlPath(singleLink,dsetter.depth); // raw split with slash as delimiter
+                if(newLevel){
+                    console.log(newLevel);
+                    console.log("Shiew");
+                    crawlScrapedLinks({url:domain+newLevel});
+                }
+            }
+        }
     }).catch(err=>{
         console.log(err);
     });
-}
+
+});
 
 
+const crawlScrapedLinks = (url,rex="/a/i",depth=3)=>{
+    crawlr({url:url,rex:rex,depth:depth});
+};
 
+crawlScrapedLinks(url,'/as/i',3);
 
 
